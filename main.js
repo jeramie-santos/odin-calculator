8661ad76a371c28a9d8cea7f37b9c5b66cfec57d
Jeramie-Santos <jeramieramsantos79@gmail.com>
2024-02-16 20:27:51 +0800

    added features, fixed errors

--- main.js
+++ main.js
@@ -22,6 +22,7 @@ let firstNum = "";
 let operator = "";
 let secondNum = "";
 let displayValue = "";
+let check;
 
 
 function getSum(num1,num2){
@@ -40,8 +41,15 @@ function getProduct(num1,num2){
 }
 
 function getQuotient(num1,num2){
-    input.textContent = num1 / num2;
-    displayValue = input.textContent;
+    let quot = num1 / num2;
+    let quotient = quot.toFixed(2);
+    if (num2 == 0){
+        input.textContent = "ERROR";
+        displayValue = "ERROR";
+    } else {
+        input.textContent = quotient;
+        displayValue = quotient;
+    }
 }
 
 function operate(){
@@ -51,14 +59,23 @@ function operate(){
     operator = operation[1];
     secondNum = Number(operation[2]);
 
-    if (operator === "+"){
-        getSum(firstNum,secondNum);
+    if (operator == null || secondNum == 0){
+        event.preventDefault();
     } else if (operator === "-"){
         getDifference(firstNum,secondNum);
     } else if (operator === "*"){
         getProduct(firstNum,secondNum);
     } else if (operator === "/"){
         getQuotient(firstNum,secondNum);
+    } else if (operator === '+'){
+        getSum(firstNum,secondNum);
+    }
+}
+
+function checkOperation(){
+    const operation = displayValue.split(" ");
+     if (operation.length == 3){
+        return true;
     }
 }
 
@@ -68,69 +85,114 @@ equal.addEventListener('click', operate);
 del.addEventListener('click', () => {
     input.textContent = "";
     displayValue = "";
+    check = false;
 });
 
 zero.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "0";
     displayValue += "0";
     console.log(displayValue);
 });
 
 one.addEventListener('click', () =>{
+    if (check == true){
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "1";
     displayValue += "1";
     console.log(displayValue);
 });
 
 two.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "2";
     displayValue += "2";
     console.log(displayValue);
 });
 
 three.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "3";
     displayValue += "3";
     console.log(displayValue);
 });
 
 four.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "4";
     displayValue += "4";
     console.log(displayValue);
 });
 
 five.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "5";
     displayValue += "5";
     console.log(displayValue);
 });
 
 six.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "6";
     displayValue += "6";
     console.log(displayValue);
 });
 
 seven.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "7";
     displayValue += "7";
     console.log(displayValue);
 });
 
 eight.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "8";
     displayValue += "8";
     console.log(displayValue);
 });
 
 nine.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += "9";
     displayValue += "9";
     console.log(displayValue);
 });
 
 decimal.addEventListener('click', () =>{
+    if (check == true) {
+        input.textContent = " ";
+        check = false;
+    }
     input.textContent += ".";
     displayValue += ".";
     console.log(displayValue);
@@ -138,28 +200,42 @@ decimal.addEventListener('click', () =>{
 
 
 
+
+
 add.addEventListener('click', () => {
+    if (checkOperation()){
+        operate();
+    }
     displayValue += " + ";
     console.log(displayValue);
-    input.textContent = " ";
+    check = true;
 });
 
 minus.addEventListener('click', () => {
+    if (checkOperation()){
+        operate();
+   }
     displayValue += " - ";
     console.log(displayValue);
-    input.textContent = " ";
+    check = true;
 });
 
 multiply.addEventListener('click', () => {
+    if (checkOperation()){
+        operate();
+   }
     displayValue += " * ";
     console.log(displayValue);
-    input.textContent = " ";
+    check = true;
 });
 
 divide.addEventListener('click', () => {
+    if (checkOperation()){
+        operate();
+   }
     displayValue += " / ";
     console.log(displayValue);
-    input.textContent = " ";
+    check = true;
 });
 
 
